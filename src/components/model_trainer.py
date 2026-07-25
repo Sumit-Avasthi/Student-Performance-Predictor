8c9c3ce94c7a8731c85ebd6a2dd4c994ea50b9f9
Sumit Avasthi <sumitavasthi.28@gmail.com>
2026-07-25 13:13:14 +0530

    some module removal

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -112,6 +112,7 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            # print(best_model_name)
 
             best_model = models[best_model_name]
 
